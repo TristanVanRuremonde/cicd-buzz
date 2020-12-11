a2c7e88973e24ee988b350ec8a906bcd8bd17b94
TristanVanRuremonde <tristan.vanruremonde@student.hu.nl>
2020-12-11 15:49:18 +0100

    second commit

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,19 +1,20 @@
-import unittest
-
 from buzz import generator
 
+
 def test_sample_single_word():
-    l = ('foo', 'bar', 'foobar')
-    word = generator.sample(l)
-    assert word in l
+    worlist = ('foo', 'bar', 'foobar')
+    word = generator.sample(worlist)
+    assert word in worlist
+
 
 def test_sample_multiple_words():
-    l = ('foo', 'bar', 'foobar')
-    words = generator.sample(l, 2)
+    wordlist = ('foo', 'bar', 'foobar')
+    words = generator.sample(wordlist, 2)
     assert len(words) == 2
-    assert words[0] in l
-    assert words[1] in l
+    assert words[0] in wordlist
+    assert words[1] in wordlist
     assert words[0] is not words[1]
+
 
 def test_generate_buzz_of_at_least_five_words():
     phrase = generator.generate_buzz()
